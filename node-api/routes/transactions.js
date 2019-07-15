0ecd801f4e23c74f5d8151c61a6b983965d31667
Sean Midford <seanmidford@outlook.com>
2019-07-15 14:23:00 -0400

    Post api for spend.

--- node-api/routes/transactions.js
+++ node-api/routes/transactions.js
@@ -71,7 +71,53 @@ router.post("/send", auth, async (req, res) => {
   }
 });
 
-router.post("/spend", auth, async (req, res) => {});
+router.post("/spend", auth, async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { amount, message } = req.body;
+
+    const user = await User.findById(_id);
+    if (!user) return res.status(400).send("Invalid user.");
+
+    const manager = await User.findById(user.managerId);
+    if (!manager) return res.status(400).send("Manager not found.");
+
+    // const { error } = validate(req.body);
+    // if (error) return res.status(400).send(error.details[0].message);
+
+    try {
+      const transaction = new Transaction({
+        kind: "spend",
+        sender: {
+          _id: user._id,
+          first: user.first,
+          last: user.last
+        },
+        message,
+        amount,
+        hash: ""
+      });
+
+      await new Fawn.Task()
+        .save("transactions", transaction)
+        .update(
+          "users",
+          { _id: user._id },
+          {
+            $inc: { balance: parseInt(amount) * -1 }
+          }
+        )
+        .run();
+
+      res.send(transaction);
+    } catch (err) {
+      res.status(500).send("Transaction failed.");
+    }
+  } catch (err) {
+    console.log(err);
+    if (err) return res.status(400).send(`Transaction was rejected.`);
+  }
+});
 
 router.post("/distribute", auth, async (req, res) => {});
 
